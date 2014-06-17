61e290c4a79743cc775a01039959a8600202cfc4
Spider Person <aranhoide@gmail.com>
2014-06-17 07:23:44 +0200

    flush for quicker log output

--- subscriber.py
+++ subscriber.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-import redis as redis_
-from pyflare import Pyflare
+import sys
 import time
+
+import redis as redis_
+
 
 def subscribe():
     redis = redis_.from_url(os.environ['REDISCLOUD_URL'])
 
+
 if __name__ == '__main__':
     for x in range(10):
         print "DONG", x
+        sys.stdout.flush()
         time.sleep(2)
