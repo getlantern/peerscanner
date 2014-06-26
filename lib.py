d0a92fcbcfcf07ea761b3bea7c0170cf0a33cdea
Ox Cart <ox.to.a.cart@gmail.com>
2014-06-26 13:19:06 -0500

    getlantern/lantern#1628 Not accepting mappings with invalid ports

--- lib.py
+++ lib.py
@@ -34,6 +34,7 @@ redis = None
 
 def register(name, ip, port):
     port = int(port)
+    print "Port is %s" % type(port)
     if port <= 0 or port > 65535:
         print "***ERROR: trying to register with invalid port: %s" % port
         return
