4af1aec3ed539470423a7d53acb550535fb94bce
Myles Horton <myles@getlantern.org>
2014-08-12 22:27:44 -0700

    failed queue debugging

--- rq_worker.py
+++ rq_worker.py
@@ -4,10 +4,12 @@ from rq import Worker, Queue, Connection
 
 import lib
 
+def exception_handler(job, exc_type, exc_value, traceback):
+	print ("ERROR job: %s, exc_type: %s, exc_value %s, traceback: %s" % (job, exc_type, exc_value, traceback))
 
 if __name__ == '__main__':
     lib.login_to_redis()
     lib.login_to_cloudflare()
     with Connection(lib.redis):
-        worker = Worker([Queue()])
+        worker = Worker([Queue()], exc_handler=exception_handler)
         worker.work()
