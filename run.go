50730fafc2cafb68196510a9e54bf37aaedb6e6a
Myles Horton <myles@getlantern.org>
2014-08-16 20:58:00 -0700

    fixed also deleting failed peers from roundrobin itself

--- run.go
+++ run.go
@@ -9,12 +9,19 @@ import (
 	"time"
 )
 
+var failedips = make([]string, 2)
+
 func main() {
 	fmt.Println("Starting CloudFlare Flashlight Tests...")
 	cf := &CloudflareApi{}
 
 	for {
+		failedips = make([]string, 2)
 		loopThroughRecords(cf)
+
+		// Make sure this gets garbage collected
+		failedips = nil
+
 		time.Sleep(6 * time.Second)
 	}
 
@@ -29,38 +36,55 @@ func loopThroughRecords(cf *CloudflareApi) {
 
 	recs := records.Response.Recs.Records
 
-	c := make(chan string)
-
-	// Now loop through all the records and try to hit each peer to
-	// see if it's up.
+	// Loop through once to hit all the peers to see if they fail.
+	c := make(chan bool)
 	numpeers := 0
 	for _, record := range recs {
-		fmt.Println("RECORD: ", record)
+		if len(record.Name) > 20 {
+			fmt.Println("PEER: ", record.Name)
+
+			go testPeer(cf, record.Domain, record.Id, record.Name, record.Value, c)
+			numpeers++
+
+		} else {
+			fmt.Println("VALUE: ", record.Value)
+		}
+	}
+
+	successes := 0
+	failures := 0
+	for i := 0; i < numpeers; i++ {
+		result := <-c
+		if result {
+			successes++
+		} else {
+			failures++
+		}
+		fmt.Println(result)
+	}
+	fmt.Printf("RESULTS:\nSUCCESES: %i\nFAILURES: %i\n", successes, failures)
+
+	fmt.Println("FAILED IPS: ", failedips)
+
+	// Now loop through again and remove any failed peers.
+	for _, record := range recs {
 		if record.Type != "A" {
 			fmt.Println("NOT AN A RECORD: ", record.Type)
 			continue
 		}
-		if len(record.Name) == 32 {
-			fmt.Println("PEER: ", record.Name)
 
-			go testPeer(cf, record.Domain, record.Id, record.Name, c)
-			numpeers++
-			//queue := mq.New("peers")
-
-			//queue.PushString()
-
-		} else {
-			fmt.Println("NOT A PEER: ", record.Name)
+		if len(record.Name) != 32 {
+			for _, ip := range failedips {
+				if record.Value == ip {
+					fmt.Println("DELETING VALUE: ", record.Value)
+					cf.remove(record.Domain, record.Id)
+				}
+			}
 		}
 	}
-
-	for i := 0; i < numpeers; i++ {
-		fmt.Println(<-c)
-	}
-
 }
 
-func testPeer(cf *CloudflareApi, domain string, id string, name string, c chan<- string) {
+func testPeer(cf *CloudflareApi, domain string, id string, name string, value string, c chan<- bool) {
 
 	client := &FlashlightClient{
 		UpstreamHost: name + ".getiantem.org"} //record.Name} //"roundrobin.getiantem.org"}
@@ -76,7 +100,8 @@ func testPeer(cf *CloudflareApi, domain string, id string, name string, c chan<-
 
 		// If it's a peer, remove it.
 		cf.remove(domain, id)
-		c <- "failure in HTTP"
+		failedips = append(failedips, value)
+		c <- false
 	} else {
 		body, err := ioutil.ReadAll(resp.Body)
 		defer resp.Body.Close()
@@ -84,10 +109,11 @@ func testPeer(cf *CloudflareApi, domain string, id string, name string, c chan<-
 			fmt.Errorf("HTTP Body Error: %s", body)
 			log.Println("Error reading body")
 			cf.remove(domain, id)
-			c <- "failure reading body"
+			failedips = append(failedips, value)
+			c <- false
 		} else {
 			log.Printf("RESPONSE FOR PEER: %s, %s", name, body)
-			c <- "success"
+			c <- true
 		}
 	}
 }
