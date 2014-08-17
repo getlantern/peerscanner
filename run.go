bdc7db973612fb0479172af7c304eabb9f3685a6
Myles Horton <myles@getlantern.org>
2014-08-16 17:43:36 -0700

    threads

--- run.go
+++ run.go
@@ -28,8 +28,11 @@ func loopThroughRecords(cf *CloudflareApi) {
 
 	recs := records.Response.Recs.Records
 
+	c := make(chan string)
+
 	// Now loop through all the records and try to hit each peer to
 	// see if it's up.
+	numpeers := 0
 	for _, record := range recs {
 		fmt.Println("RECORD: ", record)
 		if record.Type != "A" {
@@ -39,33 +42,51 @@ func loopThroughRecords(cf *CloudflareApi) {
 		if len(record.Name) == 32 {
 			fmt.Println("PEER: ", record.Name)
 
-			client := &FlashlightClient{
-				UpstreamHost: record.Name + ".getiantem.org"} //record.Name} //"roundrobin.getiantem.org"}
+			go testPeer(cf, record.Domain, record.Id, record.Name, c)
+			numpeers++
+			//queue := mq.New("peers")
 
-			httpClient := client.newClient()
+			//queue.PushString()
 
-			req, _ := http.NewRequest("GET", "http://www.google.com/humans.txt", nil)
-			resp, err := httpClient.Do(req)
-			log.Println("Finished http call")
-			if err != nil {
-				fmt.Errorf("HTTP Error: %s", resp)
-				log.Println("REMOVING RECORD FOR PEER: %s", record.Name)
-
-				// If it's a peer, remove it.
-				cf.remove(record.Domain, record.Id)
-			} else {
-				body, err := ioutil.ReadAll(resp.Body)
-				defer resp.Body.Close()
-				if err != nil {
-					fmt.Errorf("HTTP Body Error: %s", body)
-					log.Println("Returning 2")
-					continue
-				}
-
-				log.Printf("RESPONSE FOR PEER: %s, %s", record.Name, body)
-			}
 		} else {
 			fmt.Println("NOT A PEER: ", record.Name)
+		}
+	}
+
+	for i := 0; i < numpeers; i++ {
+		fmt.Print(<-c)
+	}
+
+}
+
+func testPeer(cf *CloudflareApi, domain string, id string, name string, c chan<- string) {
+
+	client := &FlashlightClient{
+		UpstreamHost: name + ".getiantem.org"} //record.Name} //"roundrobin.getiantem.org"}
+
+	httpClient := client.newClient()
+
+	req, _ := http.NewRequest("GET", "http://www.google.com/humans.txt", nil)
+	resp, err := httpClient.Do(req)
+	log.Println("Finished http call")
+	if err != nil {
+		fmt.Errorf("HTTP Error: %s", resp)
+		log.Println("REMOVING RECORD FOR PEER: %s", name)
+
+		// If it's a peer, remove it.
+		cf.remove(domain, id)
+		c <- "failure in HTTP"
+	} else {
+		body, err := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			fmt.Errorf("HTTP Body Error: %s", body)
+			log.Println("Error reading body")
+			cf.remove(domain, id)
+			c <- "failure reading body"
+		} else {
+			log.Printf("RESPONSE FOR PEER: %s, %s", name, body)
+			c <- "success"
 		}
 	}
 }
