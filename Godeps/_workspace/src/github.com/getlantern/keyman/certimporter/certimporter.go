d62d2f36814db87ed341ba5357cc84c3d07d263a
Ox Cart <ox.to.a.cart@gmail.com>
2015-01-20 21:15:32 -0600

    Upated godeps

---
+++ Godeps/_workspace/src/github.com/getlantern/keyman/certimporter/certimporter.go
@@ -0,0 +1,19 @@
+package certimporter
+
+import (
+    "fmt"
+    "reflect"
+    "unsafe"
+)
+
+func bindata_read(data, name string) ([]byte, error) {
+	var empty [0]byte
+	sx := (*reflect.StringHeader)(unsafe.Pointer(&data))
+	b := empty[:]
+	bx := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	bx.Data = sx.Data
+	bx.Len = len(data)
+	bx.Cap = bx.Len
+	return b, nil
+}
+
